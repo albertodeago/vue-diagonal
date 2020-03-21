91efac7ad7584bb28f1d2ab1421f7c5dcebe5ec5
Alberto De Agostini <albertodeagostini88@gmail.com>
2020-03-21 15:22:30 +0100

    component implementation

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>Welcome to Your Plugin in Vue.js</h1>
-    <p>add: 1 + 1 = {{ $add(1, 1) }}</p>
+    <diagonal :deg="10"
+              background="salmon"
+              space-before
+              space-after
+    >
+      My content
+    </diagonal>
+    <div style="text-align: right">
+      <h1>and this is a content after</h1>
+    </div>
   </div>
 </template>
 
 
 <style>
 #app {
+  /*text-align: center;*/
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
